d4d940a653ff6321ea3be8f614c4070c2ef7ccca
Rosen Santev <r.ssantev@gmail.com>
2023-11-27 18:36:42 +0200

    test

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
-import page from '../page/page.mjs';
-import { render } from '../lit-html/lit-html.js';
-import { userService } from './userService.js';
-import { userHelper } from './userHelper.js';
-import { loginView } from './views/loginView.js';
-import { registerView } from './views/registerView.js';
-import { browserView } from './views/quizBrowserView.js';
-import { contestView } from './views/quizContestView.js';
-import { detailsView } from './views/quizDetailsView.js';
-import { editView } from './views/quizEditorView.js';
-import { resultView } from './views/quizResultView.js';
-import { homeView } from './views/homeView.js';
-import { profileView } from './views/profileView.js';
+import page from "../page/page.mjs";
+import { render } from "../lit-html/lit-html.js";
+import { userService } from "./userService.js";
+import { userHelper } from "./userHelper.js";
+import { loginView } from "./views/loginView.js";
+import { registerView } from "./views/registerView.js";
+import { browserView } from "./views/quizBrowserView.js";
+import { contestView } from "./views/quizContestView.js";
+import { detailsView } from "./views/quizDetailsView.js";
+import { editView } from "./views/quizEditorView.js";
+import { resultView } from "./views/quizResultView.js";
+import { homeView } from "./views/homeView.js";
+import { profileView } from "./views/profileView.js";
 
 const root = document.getElementById("content");
 const userA = document.getElementById("user-nav");
 const guestA = document.getElementById("guest-nav");
 
 page(decorationContext);
-page("/", homeView);
+page("/softuni-quiz/", homeView);
 page("/login", loginView);
 page("/register", registerView);
 page("/logout", logout);
@@ -28,39 +28,38 @@ page("/create/:id", editView);
 page("/profile/:id", profileView);
 page("/result", resultView);
 
-
 page.start();
 updateNav();
 
 async function logout() {
-    await userService.logout();
-    updateNav();
-    goTo("/");
-};
-
-function renderer(template) {
-    render(template, root );
-};
-
-function updateNav () {
-    const userData = userHelper.getUserData();
-    if (userData) {
-        userA.style.display = "inline";
-        guestA.style.display = "none";
-    } else {
-        userA.style.display = "none";
-        guestA.style.display = "inline";
-    }
+  await userService.logout();
+  updateNav();
+  goTo("/");
 }
 
-function goTo (path){
-    page.redirect(path);
-};
+function renderer(template) {
+  render(template, root);
+}
+
+function updateNav() {
+  const userData = userHelper.getUserData();
+  if (userData) {
+    userA.style.display = "inline";
+    guestA.style.display = "none";
+  } else {
+    userA.style.display = "none";
+    guestA.style.display = "inline";
+  }
+}
+
+function goTo(path) {
+  page.redirect(path);
+}
 
 function decorationContext(ctx, next) {
-    ctx.render = renderer;
-    ctx.updateNav = updateNav;
-    ctx.goTo = goTo;
+  ctx.render = renderer;
+  ctx.updateNav = updateNav;
+  ctx.goTo = goTo;
 
-    next();
-};
+  next();
+}
